ca5a00c6dcc4b49cd3697b27180c292ab2151d70
Elizabeth Eidelson <fluturecode@gmail.com>
2020-12-08 13:43:01 -0500

    added styles

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Image, StyleSheet, View } from "react-native"
+
+function ViewImageScreen(props) {
+	return (
+		<View style={styles.container}>
+			<View style={styles.closeIcon}></View>
+			<Image
+				resizeMode='contain'
+				style={styles.image}
+				source={require("../assets/iphone.jpg")}
+			/>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	closeIcon: {
+		width: 50,
+		height: 50,
+		backgroundColor: "#fc5c65",
+	},
+	container: {
+		backgroundColor: "#000",
+		flex: 1,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+})
+
+export default ViewImageScreen
